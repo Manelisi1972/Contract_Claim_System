aa51d3e0e9407ef1163bcc723aa4ac2641373967
Manelisi1972 <RC_Student_Lab@LabVM1846780>
2025-10-24 20:52:12 +0200

    Modified the whole Attachment.cs class
    installed microsoft.entity.framework for database
    Create a new data folder for storing the ApplicationDbContext class for database

---
+++ Contract_Monthly_Claim_System/Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿
+using Contract_Monthly_Claim_System.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Contract_Monthly_Claim_System.Data
+{
+    public class ApplicationDbContext :  DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> opts) : base(opts) { }
+
+        public DbSet<Claim> Claims { get; set; }
+        public DbSet<Attachment> Attachments { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder mb)
+        {
+            base.OnModelCreating(mb);
+            mb.Entity<Claim>().Property(c => c.Status).HasConversion<string>().HasMaxLength(50);
+        }
+    }
+}
--- Contract_Monthly_Claim_System/Models/Attachment.cs
+++ Contract_Monthly_Claim_System/Models/Attachment.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Contract_Monthly_Claim_System.Models
 {
@@ -7,11 +8,30 @@ namespace Contract_Monthly_Claim_System.Models
     {
         [Key]
         public int AttachmentId { get; set; }
+
+        [Required]
         public int ClaimId { get; set; }
+
+        [ForeignKey("ClaimId")]
+        public Claim Claim { get; set; }
+
+        [Required]
+        [StringLength(255)]
         public string FileName { get; set; }
-        public string BlobUri { get; set; } // local path 
+
+        [Required]
+        [StringLength(50)]
+        public string FileType { get; set; }
+
         public long FileSize { get; set; }
-        public DateTime UploadedAt { get; set; } = DateTime.UtcNow;
+
+        [Required]
+        public string FilePath { get; set; } // Path to saved file
+
+        public DateTime UploadedAt { get; set; } = DateTime.Now;
+
+        [StringLength(100)]
+        public string UploadedBy { get; set; }
 
     }
 }
