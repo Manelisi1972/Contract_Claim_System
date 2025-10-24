09fab450e0574936cf429977a35459197c1eb904
Manelisi1972 <RC_Student_Lab@LabVM1846780>
2025-10-24 21:49:00 +0200

    Created a service folder  to keep controllers thin and make unit testing  easy Added an interface folder inside services folder and also added IClaimService.cs interface within interface folder.  Modified Contract_Monthly_Claim_System.csproj project

---
+++ Contract_Monthly_Claim_System/services/interfaces/IClaimService.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Contract_Monthly_Claim_System.Models;
+
+namespace Contract_Monthly_Claim_System.services.interfaces
+{
+    public interface IClaimService
+    {
+        Task<Claim> AddClaimAsync(Claim claim);
+        Task<Attachment> AddAttachmentAsync(int claimId, IFormFile file, string uploadedBy);
+        Task<IEnumerable<Attachment>> GetAttachmentsByClaimIdAsync(int claimId);
+    }
+}
