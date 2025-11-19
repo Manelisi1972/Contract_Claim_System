a4363aebd375743f5ea6047f8f03bf5828412aa1
Manelisi1972 <ST10447923@rcconnect.edu.za>
2025-11-19 21:06:48 +0200

    Added a ViewModel class in Models folder for Claim Submissions

---
+++ Contract_Monthly_Claim_System/Models/ClaimViewmodel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Contract_Monthly_Claim_System.Models
+{
+    public class ClaimViewmodel
+    {
+        [Required]
+        [Range(1, 300)]
+        public int HoursWorked { get; set; }
+
+        [Required]
+        [Range(50, 1000)]
+        public decimal HourlyRate { get; set; }
+
+        public string Notes { get; set; }
+    }
+}
