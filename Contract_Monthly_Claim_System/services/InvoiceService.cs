e4eca7dda858345ee5dd2ade23a076e10a35b049
Manelisi1972 <ST10447923@rcconnect.edu.za>
2025-11-21 18:36:19 +0200

    Added Invoice Service class for generating pdfs and reports

---
+++ Contract_Monthly_Claim_System/services/InvoiceService.cs
@@ -0,0 +1,24 @@
+﻿using Contract_Monthly_Claim_System.Data;
+using Contract_Monthly_Claim_System.Models;
+
+namespace Contract_Monthly_Claim_System.services
+{
+    public class InvoiceService
+    {
+        private readonly ApplicationDbContext _context;
+
+        public InvoiceService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public List<Claim> GetApprovedClaims(DateTime start, DateTime end)
+    {
+        return _context.Claims
+            .Where(c => c.Status == ClaimStatus.Approved &&
+                        c.ApprovedDate >= start &&
+                        c.ApprovedDate <= end)
+            .ToList();
+    }
+    }
+}
